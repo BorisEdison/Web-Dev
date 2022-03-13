0df856b2307c4c1e87f2af61c773b91a6b3eb563
Boris <borisedison@gmail.com>
2022-03-13 06:37:01 +0530

    added dice game & renamed few folders

---
+++ JavaScript/drunKit/index.js
@@ -0,0 +1,8 @@
+
+var numberOfDrums = document.querySelectorAll(".drum").length
+for (var i = 0; i < numberOfDrums ; i++){
+    document.querySelectorAll(".drun")[i].addEventListener("click", function(){
+        alert("I got clicked!")
+    
+    })
+}
